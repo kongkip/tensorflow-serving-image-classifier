24f0e7aea93b4f76360cf6389837fe4e9f727985
kongkip <evanskiplagat3@gmail.com>
2022-08-16 15:04:43 +0300

    change model input shape

--- save_model.py
+++ save_model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 
 model  = tf.keras.Sequential([ 
-    tf.keras.layers.Conv2D(32, (3, 3), input_shape=(256, 256), activation="relu"),
+    tf.keras.layers.Conv2D(32, (3, 3), input_shape=(256, 256, 3), activation="relu"),
     tf.keras.layers.MaxPooling2D((2,2)),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(3, activation="softmax")
